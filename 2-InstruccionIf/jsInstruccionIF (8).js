8a0318c375c957e095e220731c642f1af6726db9
nacho8seijas <Nachx80@gmail.com>
2018-07-12 11:52:19 -0300

    ==

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,5 +1,13 @@
 function mostrar()
 {
+    var edad;
+    var estadoCivil;
+    edad=document.getElementById("edad").value;
+    estadoCivil=document.getElementById("estadoCivil").value;
+    parseInt(edad);
+    if(edad>=18 && estadoCivil =="Soltero"){
+        alert("es soltero y no es menor" )
+    } 
 //tomo la edad  
 
 	
