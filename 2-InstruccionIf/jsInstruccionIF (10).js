4967a2ec09e6513f6a46e3949e99a4d6dab279a1
nacho8seijas <Nachx80@gmail.com>
2018-07-12 12:08:56 -0300

    fha

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,5 +1,17 @@
 function mostrar()
 {
+	var nota;
+	nota=Math.floor(Math.random()*(11-1))+1;
+	if(nota>=9){
+		alert(nota + " excelente");
+	}
+	else if(nota>=4){
+		alert(nota+ " aprobo");
+	}
+	if(nota<4){
+		alert(nota+ " vamos que se puede");
+	}
+
 	//Genero el número RANDOM entre 1 y 10 
 	
 
