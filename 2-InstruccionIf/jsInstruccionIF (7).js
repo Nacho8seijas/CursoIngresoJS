0ff93bd1ba0127deb5f33860ebaae889f559471d
nacho8seijas <Nachx80@gmail.com>
2018-07-12 11:42:29 -0300

    las mayusculas y los puntos

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,5 +1,16 @@
 function mostrar()
 {
+    var edad;
+    var estado;
+    edad=document.getElementById("edad").value;
+    estado=document.getElementById("estadoCivil").value;
+    parseInt(edad);
+    if(edad<18 && estado != "Soltero"){
+        alert("muy joven ");
+    }
+    else{
+
+    }
 //tomo la edad  
 
 	
