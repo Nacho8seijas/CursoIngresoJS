e178fb454ae05e5c4128d2a56867d083396ff57b
nacho8seijas <Nachx80@gmail.com>
2018-07-12 11:06:09 -0300

    && =>><

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,20 @@
 function mostrar()
 {
-//tomo la edad  
+    var edad
+    edad=document.getElementById("edad").value;
+    parseInt(edad);
+    if(edad >=18){
+        alert("usted es mayor")
+    }
+    if(edad >=13 && edad <=17 ){
+        alert("es adolecente")
+    
+    }
+    if(edad<13){
+        alert("usted es menor")
+
+    }
+    //tomo la edad  
 
 
 
