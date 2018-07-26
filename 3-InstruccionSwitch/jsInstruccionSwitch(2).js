88f7f08898f30c01c888d5e1f734b37be8c5c34c
Nacho8seijas <nachx80@gmail.com>
2018-07-26 10:10:03 -0300

    la llaves, los dos puntos

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,28 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+    case "Enero":
+    case "Febrero":
+    case "Diciembre":
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+        alert("Ya pasamos el invierno, ahora hace calor");
+    break;
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Febrero":
+        alert("Falta para el invierno");
+    break;
+   case "Junio":
+   case "Julio":
+   case "Agosto":
+    alert(" Abrigaate que haace frio");
+   break;
+}
 
 
 
