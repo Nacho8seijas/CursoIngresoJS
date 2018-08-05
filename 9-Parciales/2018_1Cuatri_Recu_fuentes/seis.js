d4024a46fd01b2d404802da9e6217777eefbd1e2
nacho8seijas <nachx80@gmail.com>
2018-08-05 20:23:10 -0300

    || (o) no pueden se dos numeros al mismo tiempo

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
@@ -1,25 +1,33 @@
 function mostrar()
 {
-    nota=prompt("ingrese la nota");
+    nota=document.getElementById("laHora").value;
     switch(nota)
     {
         case nota:
         if(nota<=3 && nota>=0)
         {
             alert("la proxima se puede")
+            break;
         }
         case nota:
-        if(nota>=4 && nota <=6)
-        {
+        if(nota >=4 && nota <= 6)
+        {   
             alert("raspando")
+        if(nota==4 || nota ==5 )
+        
+            alert("debes preocuparte mas")
+            break;
         }
         case nota:
         if(nota>=8)
         {
             alert("Muy bien")
+            break;
         }
+        default:nota
+        alert("Ingrese una nota valida")
+        break;
         
         
     }
-
 }
